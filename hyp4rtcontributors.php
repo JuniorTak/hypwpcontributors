420cca682e7dce298307519e4ee1e67e068d2219
Hyppolite <hyppolitak@gmail.com>
2024-04-20 02:15:11 +0100

    Add plugin file header

---
+++ hyp4rtcontributors.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package HypContributors
+ *
+ * Plugin Name: Hyp Contributors
+ * Plugin URI: https://github.com/rtlearn/wpcs-JuniorTak
+ * Description: Add contributors to blog posts
+ * Version: 1.0
+ * Author: Hyppolite Takoua Foduop
+ * Author URI: https://www.hyppolitetakouafoduop.online
+ * License: GPLv2 or later
+ * Text Domain: hypcontributors
+ */
