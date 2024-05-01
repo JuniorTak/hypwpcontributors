232b6a8fe81cbfc11c11339cad67377244ba55c5
Hyppolite <hyppolitak@gmail.com>
2024-05-01 06:27:40 +0100

    Complete method to test metabox data saving

--- tests/TestHypContributors.php
+++ tests/TestHypContributors.php
@@ -16,6 +16,12 @@ class Test_Hyp_Contributors extends WP_UnitTestCase {
 	 * @var int
 	 */
 	private $post_id;
+	/**
+	 * The user id
+	 *
+	 * @var int
+	 */
+	private $user_id;
 
 	/**
 	 * The setUp function.
@@ -23,8 +29,9 @@ class Test_Hyp_Contributors extends WP_UnitTestCase {
 	public function setUp() {
 		parent::setUp();
 
-		// Create a new post for our simulations.
+		// Create a new post and a new user for our simulations.
 		$this->post_id = $this->factory->post->create();
+		$this->user_id = $this->factory->user->create( array( 'role' => 'editor' ) );
 	}
 
 	/**
@@ -32,6 +39,7 @@ class Test_Hyp_Contributors extends WP_UnitTestCase {
 	 */
 	public function tearDown() {
 		wp_delete_post( $this->post_id, true );
+		wp_delete_user( $this->user_id );
 		parent::tearDown();
 	}
 
@@ -69,7 +77,24 @@ class Test_Hyp_Contributors extends WP_UnitTestCase {
 	/**
 	 * Save metabox data test.
 	 */
-	public function test_hyp4rt_save_metabox_data() {}
+	public function test_hyp4rt_save_metabox_data() {
+		// Set current user with proper permission.
+		wp_set_current_user( $this->user_id );
+
+		// Set up nonce.
+		$_POST['contributors_nonce'] = wp_create_nonce( 'contributors_nonce_action' );
+
+		// Simulate POST data.
+		$_POST['contributors'] = array( '1', '2' );
+
+		// Simulate the save post action.
+		do_action( 'save_post', $this->post_id );
+
+		$contributors_id = get_post_meta( $this->post_id, '_contributors', true );
+
+		// Verify metadata is saved correctly.
+		$this->assertEquals( array( '1', '2' ), $contributors_id );
+	}
 
 	/**
 	 * Display contributors test.
